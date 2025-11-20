e47b05503727b859ee25dab8a73d5a79d0a2b219
merialea <merialea5@gmail.com>
2025-11-20 16:25:08 +0300

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,33 @@
 def write_file(file_name, file_content):
-    pass
+    full_file_path = f"{file_name}.txt"
+
+    try:
+        with open(full_file_path, 'w') as file:
+            file.write(file_content)
+    except IOError as e:
+        print("Error writing to file")
+          
 
 def append_file(file_name, append_content):
-    pass
+    full_file_path = f"{file_name}.txt"
+
+    try:
+        with open(full_file_path, 'a') as file:
+            file.write(append_content)
+    except IOError as e:
+        print("Error writing to file")
 
 def read_file(file_name):
-    pass
+    full_file_path = f"{file_name}.txt"
+
+    try:
+        with open(full_file_path, 'r') as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        print(f"Error: The file {full_file_path} was not found.")
+        return None
+
+    except IOError as e:
+        print("Error reading from file")
+        return None
